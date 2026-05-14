ede9a97cfbc75076793a2b15a24516ee714e8e7f
ICYBATT <91900020+ICYBATT@users.noreply.github.com>
2026-05-14 17:29:36 +0300

    Prideti Vector konstruktoriai ir pradinis push_back metodas

--- Vector.h
+++ Vector.h
@@ -3,10 +3,11 @@
 #include <cstddef>
 #include <stdexcept>
 #include <algorithm>
+#include <initializer_list>
 
 /*
     Nuosavas Vector konteineris.
-    Pradine versija v3.0 darbui.
+    v3.0 darbo dalis.
 
     Si klase bus pleciama taip, kad funkcionalumu butu panasi i std::vector.
 */
@@ -28,9 +29,11 @@ public:
     using iterator = T*;
     using const_iterator = const T*;
 
+    // Tuscias konstruktorius
     Vector()
         : duomenys_(nullptr), dydis_(0), talpa_(0) {}
 
+    // Sukuria Vector su n tusciu elementu
     explicit Vector(size_type kiekis)
         : duomenys_(nullptr), dydis_(kiekis), talpa_(kiekis) {
         if (kiekis > 0) {
@@ -38,6 +41,7 @@ public:
         }
     }
 
+    // Sukuria Vector su n vienodu elementu
     Vector(size_type kiekis, const T& reiksme)
         : duomenys_(nullptr), dydis_(kiekis), talpa_(kiekis) {
         if (kiekis > 0) {
@@ -48,6 +52,28 @@ public:
         }
     }
 
+    // Sukuria Vector is initializer_list, pvz. Vector<int> v = {1, 2, 3}
+    Vector(std::initializer_list<T> sarasas)
+        : duomenys_(nullptr), dydis_(sarasas.size()), talpa_(sarasas.size()) {
+        if (talpa_ > 0) {
+            duomenys_ = new T[talpa_];
+            size_type i = 0;
+            for (const auto& elementas : sarasas) {
+                duomenys_[i++] = elementas;
+            }
+        }
+    }
+
+    // Sukuria Vector is iteratoriu intervalo
+    template <typename InputIterator>
+    Vector(InputIterator pirmas, InputIterator paskutinis)
+        : duomenys_(nullptr), dydis_(0), talpa_(0) {
+        for (InputIterator it = pirmas; it != paskutinis; ++it) {
+            push_back(*it);
+        }
+    }
+
+    // Destruktorius
     ~Vector() {
         delete[] duomenys_;
     }
@@ -86,6 +112,22 @@ public:
         return duomenys_[indeksas];
     }
 
+    reference front() {
+        return duomenys_[0];
+    }
+
+    const_reference front() const {
+        return duomenys_[0];
+    }
+
+    reference back() {
+        return duomenys_[dydis_ - 1];
+    }
+
+    const_reference back() const {
+        return duomenys_[dydis_ - 1];
+    }
+
     iterator begin() {
         return duomenys_;
     }
@@ -100,5 +142,23 @@ public:
 
     const_iterator end() const {
         return duomenys_ + dydis_;
+    }
+
+    void push_back(const T& reiksme) {
+        if (dydis_ == talpa_) {
+            size_type nauja_talpa = (talpa_ == 0) ? 1 : talpa_ * 2;
+            T* nauji_duomenys = new T[nauja_talpa];
+
+            for (size_type i = 0; i < dydis_; ++i) {
+                nauji_duomenys[i] = duomenys_[i];
+            }
+
+            delete[] duomenys_;
+            duomenys_ = nauji_duomenys;
+            talpa_ = nauja_talpa;
+        }
+
+        duomenys_[dydis_] = reiksme;
+        ++dydis_;
     }
 };
